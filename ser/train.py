7c39f2758c5379c8a355af5a4e6f0bf58d36149e
isobel16 <91728519+isobel16@users.noreply.github.com>
2021-11-23 14:37:09 +0000

    my solutions for day 2

--- ser/train.py
+++ ser/train.py
@@ -8,9 +8,8 @@ from torchvision import datasets, transforms
 
 import typer
 
-def train_model(epochs, training_dataloader, validation_dataloader, model, optimizer, device):
-    for epoch in range(epochs):
-        for i, (images, labels) in enumerate(training_dataloader):
+def training_batch(epoch, training_dataloader, model, optimizer, device):
+    for i, (images, labels) in enumerate(training_dataloader):
             images, labels = images.to(device), labels.to(device)
             model.train()
             optimizer.zero_grad()
@@ -20,22 +19,30 @@ def train_model(epochs, training_dataloader, validation_dataloader, model, optim
             optimizer.step()
             print(
                 f"Train Epoch: {epoch} | Batch: {i}/{len(training_dataloader)} "
-                f"| Loss: {loss.item():.4f}"
-            )
-            # validate
-            val_loss = 0
-            correct = 0
-            with torch.no_grad():
-                for images, labels in validation_dataloader:
-                    images, labels = images.to(device), labels.to(device)
-                    model.eval()
-                    output = model(images)
-                    val_loss += F.nll_loss(output, labels, reduction="sum").item()
-                    pred = output.argmax(dim=1, keepdim=True)
-                    correct += pred.eq(labels.view_as(pred)).sum().item()
-                val_loss /= len(validation_dataloader.dataset)
-                val_acc = correct / len(validation_dataloader.dataset)
+                f"| Loss: {loss.item():.4f}" )
 
-                print(
-                    f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
+
+
+        # validate
+def val_epoch(epoch, validation_dataloader, model, device):
+        val_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for images, labels in validation_dataloader:
+                images, labels = images.to(device), labels.to(device)
+                model.eval()
+                output = model(images)
+                val_loss += F.nll_loss(output, labels, reduction="sum").item()
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(labels.view_as(pred)).sum().item()
+            val_loss /= len(validation_dataloader.dataset)
+            val_acc = correct / len(validation_dataloader.dataset)
+
+            print(
+                f"Val Epoch: {epoch} | Avg Loss: {val_loss:.4f} | Accuracy: {val_acc}"
                 )
+
+def train_model(epochs, training_dataloader, validation_dataloader model, optimizer, device):
+    for epoch in range(epochs):
+        training_batch(epoch, training_dataloader, model, optimizer, device)
+        val_epoch(epoch, validation_dataloader, model, optimizer, device)
